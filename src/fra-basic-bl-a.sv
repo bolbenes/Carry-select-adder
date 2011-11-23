5b2bc790a095aadc0e241d1f91d46b757559f04b
Guillaume Bolbenes <bolbenes@telecom-paristech.fr>
2011-11-23 18:17:04 +0100

    le makefile marche

--- src/fra-basic-bl-a.sv
+++ src/fra-basic-bl-a.sv
@@ -1,5 +1,15 @@
-module fra-basic-bl-a (input a, input b, input p, input c1, input c2, output c3, output c4, output s);
-	assign c4 = ~(a and b);
-	assign c3 = ~(c1 and (c2 and p));
-	assign s = p ^ (c1 and c2);
+module fra_basic_bl_a (
+		       input a,
+		       input b,
+		       input p,
+		       input c1,
+		       input c2,
+		       output c3,
+		       output c4,
+		       output s);
+
+	assign c4 = ~(a & b);
+	assign c3 = ~(c1 & (c2 & p));
+	assign s = p ^ (c1 & c2);
+
 endmodule
--- src/fra-basic-bl-b.sv
+++ src/fra-basic-bl-b.sv
@@ -1,5 +1,15 @@
-module fra-basic-bl-a (input a, input b, input c3, input c4, output c1, output c2, output s);
-	assign c1 = ~(c3 and (p and c4));
-	assign c2 = a or b;
-	assign s = p ^ ~(c3 and c4);
+module fra_basic_bl_b (
+		       input a,
+		       input b,
+		       input p,
+		       input c3,
+		       input c4,
+		       output c1,
+		       output c2,
+		       output s);
+ 
+	assign c1 = ~(c3 & (p & c4));
+	assign c2 = a | b;
+	assign s = p ^ ~(c3 & c4);
+
 endmodule
